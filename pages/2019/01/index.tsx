e166ac2f1302a96912d670ff0cfc6f8a50be25dd
arkteck <arkteck@gmail.com>
2023-02-24 10:17:25 -0500

    Testing page routing

---
+++ pages/2019/01/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+function Page() {
+  return (<>
+    <div>Something happened?!</div>
+    <Link href="/">Go back!</Link>
+  </>)
+}
+
+export default Page
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+
 function HomePage() {
-  return <div>Welcome to Next.js! Typescript version</div>
+  return (<>
+    <div>Welcome to Next.js! Typescript version</div>
+    <Link href="/2019/01">Solution for day 2019/01</Link>
+  </>)
 }
 
 export default HomePage
