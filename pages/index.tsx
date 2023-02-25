18aab49ef042f24e0bd5272a82507db12979610f
arkteck <arkteck@gmail.com>
2023-02-25 10:08:24 -0500

    Testing accordion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
 import Link from 'next/link'
+import { Accordion } from 'react-bootstrap'
 
 function HomePage() {
+  const years = [];
+  for (let i = 2015; i <= 2022; i++) {
+    years.push(i);
+  };
+
+  const days = [];
+  for (let i = 1; i <= 25; i++) {
+    days.push(i);
+  };
+
   return (<>
-    <div>Welcome to Next.js! Typescript version</div>
-    <Link href="/2019/01">Solution for day 2019/01</Link>
+    <div>Advent of Code Solvers (for some days)</div>
+    <Accordion>
+      {years.map((year) => (
+        <Accordion.Item key={year} eventKey={year}>
+          <Accordion.Header>
+            {year}
+          </Accordion.Header>
+          {days.map((day) => (
+            <Accordion.Body key={day}>
+              <Link href={`/${year}/${day}`}>
+                {day}
+              </Link>
+            </Accordion.Body>
+          ))}
+        </Accordion.Item>
+      ))}
+    </Accordion>
   </>)
 }
 
