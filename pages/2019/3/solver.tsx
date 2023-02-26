5e51d63edc39e89f8ddd04b25735bfd9e0efe696
arkteck <arkteck@gmail.com>
2023-02-26 09:42:31 -0500

    Day 2019/3 part 2 solved

--- pages/2019/3/solver.tsx
+++ pages/2019/3/solver.tsx
@@ -1,11 +1,12 @@
 export default function Solver(inputAOC : string) : string[] {
   const parsedInput = inputAOC.split('\n').map((a) => a.split(','));
   let part1 = Infinity;
-  let part2 = 0;
+  let part2 = Infinity;
 
   const pos = [0, 0];
-  const wirepath = new Set(['0.0']);
+  const wirepath = new Map();
   const re = new RegExp(`^([UDLR])(\\d+)$`)
+  let steps = 0;
 
   for (const path of parsedInput[0]) {
     const matches = path.match(re);
@@ -24,14 +25,19 @@ export default function Solver(inputAOC : string) : string[] {
       dir[0] = 1;
     }
     for (let i = 0; i < dist; i++) {
+      steps++;
       pos[0] += dir[0];
       pos[1] += dir[1];
-      wirepath.add(pos[0] + '.' + pos[1]);
+      const posStr = pos[0] + '.' + pos[1];
+      if (!wirepath.has(posStr)) {
+        wirepath.set(posStr, steps);
+      }
     }
   }
 
   pos[0] = 0;
   pos[1] = 0;
+  steps = 0;
 
   for (const path of parsedInput[1]) {
     const matches = path.match(re);
@@ -50,12 +56,16 @@ export default function Solver(inputAOC : string) : string[] {
       dir[0] = 1;
     }
     for (let i = 0; i < dist; i++) {
+      steps++;
       pos[0] += dir[0];
       pos[1] += dir[1];
-      if (wirepath.has(pos[0] + '.' + pos[1])) {
+      const posStr = pos[0] + '.' + pos[1];
+      if (wirepath.has(posStr)) {
         part1 = Math.min(part1, Math.abs(pos[0]) + Math.abs(pos[1]));
+        part2 = Math.min(part2, wirepath.get(posStr) + steps);
       }
     }
   }
+
   return [part1.toString(), part2.toString()];
 }
