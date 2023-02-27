5617db3c1909cc4bc9b5d14e08a2e193430e5161
arkteck <arkteck@gmail.com>
2023-02-27 10:54:53 -0500

    Solved day 2019/5 part 2

--- pages/2019/5/solver.tsx
+++ pages/2019/5/solver.tsx
@@ -3,9 +3,9 @@ export default function Solver(inputAOC : string) : string[] {
   if (parsedInput.some((a) => Number.isNaN(a))) {
     return ['ERROR'];
   }
+  const parsedInput2 = parsedInput.slice();
 
   let part1 = 0;
-  let part2 = 0;
   let singleInput = 1;
 
   let i = 0;
@@ -27,7 +27,7 @@ export default function Solver(inputAOC : string) : string[] {
       parsedInput[parsedInput[i + 1]] = singleInput;
       i++;
     } else if (opcode === 4) {
-      part1 =  parsedInput[parsedInput[i + 1]];
+      part1 = parsedInput[parsedInput[i + 1]];
       i++;
     } else {
       let value1 = 0;
@@ -53,6 +53,94 @@ export default function Solver(inputAOC : string) : string[] {
 
       if (param3 === 0) {
         parsedInput[parsedInput[i + 3]] = value3;
+      } else if (param3 === 1) {
+        console.log('Writing in immediate mode?');
+        break;
+      }
+      i += 3;
+    }
+
+    i++;
+
+  }
+
+  let part2 = 0;
+  let singleInput2 = 5;
+
+  i = 0;
+
+  while (i < parsedInput2.length) {
+    let instruc = parsedInput2[i];
+    const opcode = instruc % 100;
+    instruc = Math.floor(instruc / 100);
+    const param1 = instruc % 10;
+    instruc = Math.floor(instruc / 10);
+    const param2 = instruc % 10;
+    instruc = Math.floor(instruc / 10);
+    const param3 = instruc % 10;
+
+    if (opcode === 99) {
+      break;
+    }
+    if (opcode === 3) {
+      if (param1 === 0) {
+        parsedInput2[parsedInput2[i + 1]] = singleInput2;
+      } else if (param1 === 1) {
+        parsedInput2[i + 1] = singleInput2;
+      }
+      i++;
+    } else if (opcode === 4) {
+      if (param1 === 0) {
+        part2 = parsedInput2[parsedInput2[i + 1]];
+      } else if (param1 === 1) {
+        part2 = parsedInput2[i + 1];
+      }
+      i++;
+    } else {
+      let value1 = 0;
+      if (param1 === 0) {
+        value1 = parsedInput2[parsedInput2[i + 1]];
+      } else if (param1 === 1) {
+        value1 = parsedInput2[i + 1];
+      }
+
+      let value2 = 0;
+      if (param2 === 0) {
+        value2 = parsedInput2[parsedInput2[i + 2]];
+      } else if (param2 === 1) {
+        value2 = parsedInput2[i + 2];
+      }
+
+      let value3 = 0;
+      if (opcode === 1) {
+        value3 = value1 + value2;
+      } else if (opcode === 2) {
+        value3 = value1 * value2;
+      } else{
+        if (opcode === 5) {
+          if (value1 !== 0) {
+            i = value2;
+          } else {
+            i += 3;
+          }
+        } else if (opcode === 6) {
+          if (value1 === 0) {
+            i = value2;
+          } else {
+            i += 3;
+          }
+        } else if (opcode === 7) {
+          parsedInput2[parsedInput2[i + 3]] = value1 < value2 ? 1 : 0;
+          i += 4;
+        } else if (opcode === 8) {
+          parsedInput2[parsedInput2[i + 3]] = value1 === value2 ? 1 : 0;
+          i += 4;
+        }
+        continue;
+      }
+
+      if (param3 === 0) {
+        parsedInput2[parsedInput2[i + 3]] = value3;
       } else if (param3 === 1) {
         console.log('Writing in immediate mode?');
         break;
