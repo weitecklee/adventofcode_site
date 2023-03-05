978d6f7060acdb23c565eac41746c4d3a8ac1b6d
arkteck <arkteck@gmail.com>
2023-03-05 15:14:31 -0500

    Day 2019/10 part 2 solved

--- pages/2019/10/solver.tsx
+++ pages/2019/10/solver.tsx
@@ -5,6 +5,8 @@ const gcd = (a : number, b : number) : number => {
   return gcd(b, a % b);
 }
 
+const dist = (a : number, b : number, c : number, d : number) : number => (Math.sqrt((a - c) ** 2 + (b - d) ** 2));
+
 export default function Solver(inputAOC : string) : string[] {
   const parsedInput = inputAOC.split('\n');
 
@@ -21,25 +23,104 @@ export default function Solver(inputAOC : string) : string[] {
     }
   }
 
+  let x : number
+  let y : number
+
   for (const [a, b] of asteroids) {
     const lineOfSight : Set<string> = new Set();
     for (const [i, j] of asteroids) {
-      if (a !== i) {
-        const g = gcd(Math.abs(i - a), Math.abs(j - b));
-        lineOfSight.add(`${(i - a) / g},${(j - b) / g}`)
-      } else if (b !== j) {
-        if (j > b) {
-          lineOfSight.add('Inf');
-        } else {
-          lineOfSight.add('-Inf');
-        }
+      if (a === i && b === j) {
+        continue;
       }
+      const g = gcd(Math.abs(i - a), Math.abs(j - b));
+      lineOfSight.add(`${(i - a) / g},${(j - b) / g}`)
     }
     if (lineOfSight.size > part1) {
-      // console.log(a, b, lineOfSight.size);
+      x = a;
+      y = b;
       part1 = lineOfSight.size;
     }
   }
+
+  const map : Map<string,number[][]> = new Map();
+
+  for (const [a, b] of asteroids) {
+    let slope = '';
+    if (a === x && b === y) {
+      continue;
+    }
+    const g = gcd(Math.abs(x - a), Math.abs(y - b));
+    slope = `${(a - x) / g},${(y - b) / g}`;
+    if (!map.has(slope)) {
+      map.set(slope, []);
+    }
+    map.get(slope).push([a, b]);
+  }
+
+  const asteroidLines : number[][][] = [];
+  for (const [slope, arr] of map) {
+    arr.sort((a, b) =>  dist(b[0], b[1], x, y) - dist(a[0], a[1], x, y));
+    const [a, b] = slope.split(',').map(Number);
+    if (a === 0) {
+      if (b > 0) {
+        arr.push([0]);
+      } else {
+        arr.push([Math.PI]);
+      }
+    } else if ( b === 0) {
+      if (a > 0) {
+        arr.push([Math.PI / 2]);
+      } else {
+        arr.push([3 * Math.PI / 2]);
+      }
+
+    } else {
+      if (a > 0) {
+        if (b > 0) {
+          arr.push([Math.atan(a / b)]);
+        } else {
+          arr.push([Math.atan(-b / a) + Math. PI / 2]);
+        }
+      } else {
+        if (b > 0) {
+          arr.push([Math.atan(-b / a) + 3 * Math.PI / 2]);
+        } else {
+          arr.push([Math.atan(a / b) + Math. PI]);
+        }
+      }
+    }
+    asteroidLines.push(arr);
+  }
+
+  const vaporize = [];
+
+  for (const arr of asteroidLines.values()) {
+    vaporize.push(arr);
+  }
+
+  vaporize.sort((a, b) => a[a.length - 1][0] - b[b.length - 1][0]);
+  vaporize.forEach((a) => a.pop());
+
+  let i = 0;
+  let k = 0;
+  let lastVaporized : number[] = [];
+
+  while (i < 200) {
+    while (vaporize[k].length === 0) {
+      k++;
+      if (k >= vaporize.length) {
+        k = 0;
+      }
+    }
+    i++;
+    lastVaporized = vaporize[k].pop();
+    k++;
+    if (k >= vaporize.length) {
+      k = 0;
+    }
+  }
+
+  part2 = lastVaporized[0] * 100 + lastVaporized[1];
 
   return [part1.toString(), part2.toString()];
 }
