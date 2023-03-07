457aee42d88545e85c7cb766fea741ff382620ad
arkteck <arkteck@gmail.com>
2023-03-07 14:14:39 -0500

    Day 2019/16 part 2 solved

--- pages/2019/16/solver.tsx
+++ pages/2019/16/solver.tsx
@@ -1,8 +1,10 @@
 export default function Solver(inputAOC: string): string[] {
   let parsedInput = inputAOC.split('').map(Number);
+  const parsedInput2 = parsedInput.slice();
+  const offset = Number(inputAOC.slice(0, 7));
 
   let part1 = '';
-  let part2 = 0;
+  let part2 = '';
 
   for (let phase = 0; phase < 100; phase++) {
     const newInput: number[] = [];
@@ -24,6 +26,27 @@ export default function Solver(inputAOC: string): string[] {
 
   part1 = parsedInput.join('').slice(0, 8);
 
-  return [part1.toString(), part2.toString()];
+  /*
+  For part 2, the trick is that for any index i of signal of length n,
+  if i >= n / 2, then signal_updated[i] = sum(signal[i:]) % 10.
+  Simply iterate from the end to the offset. The offset seems to always be
+  greater than n / 2 so that this property can be taken advantage of.
+  */
+
+  for (let i = 0; i < 10000 - 1; i++) {
+    for (let j = 0; j < parsedInput.length; j++) {
+      parsedInput2.push(parsedInput2[j]);
+    }
+  }
+
+  for (let phase = 0; phase < 100; phase++) {
+    for (let i = parsedInput2.length - 1; i >= offset; i--) {
+      parsedInput2[i] = (parsedInput2[i] + (parsedInput2[i + 1] ?? 0)) % 10;
+    }
+  }
+
+  part2 = parsedInput2.join('').slice(offset, offset + 8);
+
+  return [part1, part2];
 }
 
--- pages/2019/16/testInput.tsx
+++ pages/2019/16/testInput.tsx
@@ -1,3 +1,4 @@
-const input = `12345678`;
+// const input = `12345678`;
+const input = `03036732577212944063491565474664`;
 
 export default input;
