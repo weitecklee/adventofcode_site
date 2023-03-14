6d93cb57b7e8626205a8c9ea04689a382319c3d9
arkteck <arkteck@gmail.com>
2023-03-14 10:28:11 -0400

    Day 2018/1 part 2 solved

--- pages/2018/1/solver.tsx
+++ pages/2018/1/solver.tsx
@@ -1,4 +1,4 @@
-export default function Solver(inputAOC : string) : string[] {
+export default function Solver(inputAOC: string): string[] {
   const parsedInput = inputAOC.split('\n');
 
   let part1 = 0;
@@ -6,6 +6,17 @@ export default function Solver(inputAOC : string) : string[] {
 
   for (const line of parsedInput) {
     part1 += Number(line)
+  }
+
+  const freqSet: Set<number> = new Set();
+  let row = 0;
+  while (!freqSet.has(part2)) {
+    freqSet.add(part2);
+    part2 += Number(parsedInput[row]);
+    row++;
+    if (row > parsedInput.length - 1) {
+      row = 0;
+    }
   }
 
   return [part1.toString(), part2.toString()];
