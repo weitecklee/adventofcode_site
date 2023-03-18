d034846987fa5b1976e83286593048e182fa5159
arkteck <arkteck@gmail.com>
2023-03-18 10:03:27 -0400

    Day 2018/7 part 2 solved

--- pages/2018/7/solver.tsx
+++ pages/2018/7/solver.tsx
@@ -34,6 +34,54 @@ export default function Solver(inputAOC: string): string[] {
     prereqs.forEach((set) => set.delete(stepDone));
   }
 
+  for (const line of parsedInput) {
+    const matches = line.match(re);
+    if (!prereqs.has(matches[1])) {
+      prereqs.set(matches[1], new Set());
+    }
+    if (!prereqs.has(matches[2])) {
+      prereqs.set(matches[2], new Set());
+    }
+    prereqs.get(matches[2]).add(matches[1]);
+  }
+
+  class Worker {
+    finishTime: number;
+    finishStep: string;
+
+    constructor(time: number, step: string) {
+      this.finishTime = time;
+      this.finishStep = step;
+    }
+  }
+  const workers: Worker[] = [];
+
+  for (const [step, set] of prereqs) {
+    if (set.size === 0) {
+      stepsToBeDone.push(step);
+      prereqs.delete(step);
+    }
+  }
+  stepsToBeDone.sort((a, b) => a < b ? 1 : a > b ? -1 : 0);
+
+  while (prereqs.size || stepsToBeDone.length || workers.length) {
+    if (stepsToBeDone.length === 0 || workers.length === 5) {
+      const finishedWorker = workers.shift();
+      part2 = finishedWorker.finishTime;
+      prereqs.forEach((set) => set.delete(finishedWorker.finishStep));
+      for (const [step, set] of prereqs) {
+        if (set.size === 0) {
+          stepsToBeDone.push(step);
+          prereqs.delete(step);
+        }
+      }
+      stepsToBeDone.sort((a, b) => a < b ? 1 : a > b ? -1 : 0);
+    } else {
+      const stepDone = stepsToBeDone.pop();
+      workers.push(new Worker(part2 + stepDone.charCodeAt(0) - 4, stepDone))
+    }
+  }
+
   return [part1, part2.toString()];
 }
 
